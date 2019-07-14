0768749ce1436bca879ac18e2542d056bb292d66
syliesox <syliesox@gmail.com>
2019-07-13 21:35:08 -0400

    added build folder and updated mongo URL info

---
+++ client/build/precache-manifest.ece52573534aad664079246c586240f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a70cfc78b42885ea40c",
+    "url": "/static/js/main.6b1def4d.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "5a70cfc78b42885ea40c",
+    "url": "/static/css/main.2296c18c.chunk.css"
+  },
+  {
+    "revision": "34ff9b3cc0bab54063e08403cf1cc2a7",
+    "url": "/index.html"
+  }
+];
